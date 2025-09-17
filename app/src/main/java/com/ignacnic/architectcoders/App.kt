9f7f220810e5684fbabe1777471440d3cffce9d1
Ignacio Nicolas Lopez <i.nicolaslop@gmail.com>
2025-09-17 11:21:48 +0200

    ACP-24: Practica 5 (#16)

--- app/src/main/java/com/ignacnic/architectcoders/App.kt
+++ app/src/main/java/com/ignacnic/architectcoders/App.kt
@@ -1,11 +1,24 @@
 package com.ignacnic.architectcoders
 
 import android.app.Application
-import com.ignacnic.architectcoders.di.Initializer
+import com.ignacnic.architectcoders.di.appModule
+import com.ignacnic.architectcoders.di.repositoryModule
+import com.ignacnic.architectcoders.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-        Initializer.app = this
+        startKoin {
+            androidLogger()
+            androidContext(androidContext = this@App)
+            modules(
+                appModule,
+                repositoryModule,
+                viewModelModule,
+            )
+        }
     }
 }
