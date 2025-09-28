9641a3fa20c1f60c7865bc4a698e5836452d8e1f
Ignacio Nicolas Lopez <i.nicolaslop@gmail.com>
2025-09-28 17:58:47 +0200

    ACP-27 Add snapshot tests (#19)

--- common/ui-resources/build.gradle.kts
+++ common/ui-resources/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.ignacnic.android.library)
     alias(libs.plugins.ignacnic.compose)
+    alias(libs.plugins.ignacnic.test.screenshot)
+    alias(libs.plugins.paparazzi)
 }
 
 android {
